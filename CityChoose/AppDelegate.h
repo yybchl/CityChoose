cae53c00906b621779ae033607cdacd8d33013c6
杨友彬 <987311160@qq.com>
2016-05-13 15:51:40 +0800

    Initial Commit

---
+++ CityChoose/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CityChoose
+//
+//  Created by YoYo on 16/5/13.
+//  Copyright © 2016年 cn.yoyoy.mw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CityChoose/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CityChoose
+//
+//  Created by YoYo on 16/5/13.
+//  Copyright © 2016年 cn.yoyoy.mw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
