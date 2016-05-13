626b66a03c163674974631ec960ac7d0454cf57e
杨友彬 <987311160@qq.com>
2016-05-13 17:03:32 +0800

    城市选择

--- CityChoose/ViewController.m
+++ CityChoose/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "YoYoCityChoose/CityChooseViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *province;
+@property (strong, nonatomic) IBOutlet UILabel *area;
 
 @end
 
@@ -17,6 +20,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)cityChooseAction:(id)sender {
+    
+    //城市选择
+    CityChooseViewController *vc = [CityChooseViewController new];
+    //选择以后的回调
+    [vc returnCityInfo:^(NSString *province, NSString *area) {
+        _province.text = province; //选择的省
+        _area.text = area; //选择的地区
+    }];
+    [self.navigationController pushViewController:vc animated:YES];
+    
 }
 
 - (void)didReceiveMemoryWarning {
